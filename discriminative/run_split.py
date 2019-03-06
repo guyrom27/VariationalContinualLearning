466ceeca251cde91e62fcddc2a95399877d86ec6
PierreAlexSW <pierrealex1995@gmail.com>
2019-03-06 18:08:03 +0000

    cleaned up a bit

--- discriminative/run_split.py
+++ discriminative/run_split.py
@@ -6,53 +6,9 @@ sys.path.extend(['alg/'])
 import discriminative.vcl as vcl
 import discriminative.coreset as coreset
 import discriminative.utils as utils
+from discriminative.DataGenerator import SplitMnistGenerator
 from copy import deepcopy
 
-class SplitMnistGenerator():
-    def __init__(self):
-        with gzip.open('data/mnist.pkl.gz', 'rb') as file:
-            u = cp._Unpickler(file)
-            u.encoding = 'latin1'
-            p = u.load()
-            train_set, valid_set, test_set = p
-
-        self.X_train = np.vstack((train_set[0], valid_set[0]))
-        self.X_test = test_set[0]
-        self.train_label = np.hstack((train_set[1], valid_set[1]))
-        self.test_label = test_set[1]
-
-        self.sets_0 = [0, 2, 4, 6, 8]
-        self.sets_1 = [1, 3, 5, 7, 9]
-        self.max_iter = len(self.sets_0)
-        self.cur_iter = 0
-
-    def get_dims(self):
-        # Get data input and output dimensions
-        return self.X_train.shape[1], 2
-
-    def next_task(self):
-        if self.cur_iter >= self.max_iter:
-            raise Exception('Number of tasks exceeded!')
-        else:
-            # Retrieve train data
-            train_0_id = np.where(self.train_label == self.sets_0[self.cur_iter])[0]
-            train_1_id = np.where(self.train_label == self.sets_1[self.cur_iter])[0]
-            next_x_train = np.vstack((self.X_train[train_0_id], self.X_train[train_1_id]))
-
-            next_y_train = np.vstack((np.ones((train_0_id.shape[0],1 )), np.zeros((train_1_id.shape[0],1 )))).squeeze(-1)
-            #next_y_train = np.hstack((next_y_train, 1-next_y_train))
-
-            # Retrieve test data
-            test_0_id = np.where(self.test_label == self.sets_0[self.cur_iter])[0]
-            test_1_id = np.where(self.test_label == self.sets_1[self.cur_iter])[0]
-            next_x_test = np.vstack((self.X_test[test_0_id], self.X_test[test_1_id]))
-
-            next_y_test = np.vstack((np.ones((test_0_id.shape[0],1 )), np.zeros((test_1_id.shape[0], 1)))).squeeze(-1)
-            #next_y_test = np.hstack((next_y_test, 1-next_y_test))
-
-            self.cur_iter += 1
-
-            return next_x_train, next_y_train, next_x_test, next_y_test
 
 hidden_size = [256, 256]
 batch_size = None
