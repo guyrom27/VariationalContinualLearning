Fixed increasing loss bug- changed KL calc from tensor.item() to tensor.squeeze() hence now the gradient is correct.\n changed the update_prior to change the log_sigma to -6 as per original code. added prints in both versions and changed to batch size of 50 and 6 epochs for both

generative/alg/onlinevi.py
@@ -108,7 +108,8 @@ def construct_optimizer(X_ph, enc, dec, ll, N_data, batch_size_ph, shared_prior_
     opt = tf.train.AdamOptimizer(learning_rate=lr_ph).minimize(loss_total)
     
     ops = [opt, loss_total, my_logp, my_kl_z, bound, kl_theta]
-    def train(sess, X, lr):
+    def train(sess, X, lr, prints = False):
+
 
         #for i, layer in enumerate(dec.shared_net.layers):
         #    print("DEC_shared mu_weigkl_thetahts",i,  sess.run(layer.mu_W))
@@ -116,21 +117,23 @@ def construct_optimizer(X_ph, enc, dec, ll, N_data, batch_size_ph, shared_prior_
         #   print("DEC_shared log_sig_weights",i,  sess.run(layer.log_sig_W))
         #   print("DEC_shared log_sig_bias",i,  sess.run(layer.log_sig_b))
 
-
-        #print()
-        #print("Log_like", sess.run(tf.reduce_mean(my_logp),feed_dict={X_ph: X, lr_ph: lr,
-        #                            batch_size_ph: X.shape[0]}))
-        #print("KL Z", sess.run(tf.reduce_mean(my_kl_z),feed_dict={X_ph: X, lr_ph: lr,
-        #                            batch_size_ph: X.shape[0]}))
-        #print("Log_like - KL Z", sess.run(tf.reduce_mean(my_logp-my_kl_z), feed_dict={X_ph: X, lr_ph: lr,
-        #                                                               batch_size_ph: X.shape[0]}))
-        #print("Bound", sess.run(bound, feed_dict={X_ph: X, lr_ph: lr,
-        #                                                           batch_size_ph: X.shape[0]}))
-        #print("KL Qt vs prev Qt", sess.run(kl_theta,feed_dict={X_ph: X, lr_ph: lr,
-        #                            batch_size_ph: X.shape[0]})/N_data)
+        if prints:
+            print()
+            Log_like =  sess.run(tf.reduce_mean(my_logp),feed_dict={X_ph: X, lr_ph: lr,
+                                        batch_size_ph: X.shape[0]})
+            KL_Z = sess.run(tf.reduce_mean(my_kl_z),feed_dict={X_ph: X, lr_ph: lr,
+                                        batch_size_ph: X.shape[0]})
+            Log_like_KL_Z = sess.run(tf.reduce_mean(my_logp-my_kl_z), feed_dict={X_ph: X, lr_ph: lr,
+                                                                           batch_size_ph: X.shape[0]})
+            Bound =  sess.run(bound, feed_dict={X_ph: X, lr_ph: lr,
+                                                                       batch_size_ph: X.shape[0]})
+            KL_Qt_vs_prev_Qt =  sess.run(kl_theta,feed_dict={X_ph: X, lr_ph: lr,
+                                        batch_size_ph: X.shape[0]})/N_data
+            print ("ll,\t     klz,\tll-klz,\tbound,\t klq_t2qt-1")
+            print(int(Log_like),'\t', int(KL_Z),'\t','\t', int(Log_like_KL_Z),'\t', int(Bound),'\t', int(KL_Qt_vs_prev_Qt))
 
         _, my_loss_total, my_my_logp, my_my_kl_z, my_bound, my_kl_theta = sess.run(ops, feed_dict={X_ph: X, lr_ph: lr,
-                                    batch_size_ph: X.shape[0]})
+                                        batch_size_ph: X.shape[0]})
 
         return my_loss_total, my_bound, my_kl_theta / N_data
 
@@ -143,16 +146,19 @@ def construct_optimizer(X_ph, enc, dec, ll, N_data, batch_size_ph, shared_prior_
             ind_s = np.random.permutation(list(range(N)))
             bound_total = 0.0
             kl_total = 0.0
+            print_counter = 0
             for j in range(0, n_iter_vae):
                 indl = j * batch_size
                 indr = (j+1) * batch_size
                 ind = ind_s[indl:min(indr, N)]
                 if indr > N:
                     ind = np.concatenate((ind, ind_s[:(indr-N)]))
-                my_loss_total, logp, kl = train(sess, X[ind], lr)
+                my_loss_total, logp, kl = train(sess, X[ind], lr, print_counter % 20 ==18)
                 bound_total += logp / n_iter_vae
                 kl_total += kl / n_iter_vae
-                print("Loss on iteration", j, my_loss_total)
+                print_counter +=1
+                if (print_counter % 20 ==19):
+                    print("Loss on iteration", j, my_loss_total)
             end = time.time()
             print("Iter %d, bound=%.2f, kl=%.2f, time=%.2f" \
                   % (iteration, bound_total, kl_total, end - begin))

sandbox/torch_sandbox.py
@@ -36,8 +36,8 @@ loss_print = False
 dimX = 28 * 28
 dimH = 500
 dimZ = 50
-batch_size = 20
-n_epochs = 1 # 200
+batch_size = 50
+n_epochs = 6 # 200
 
 # Shared decoder
 dec_shared_dims = [dimH, dimH, dimX]
@@ -173,17 +173,21 @@ class SharedDecoder(nn.Module):
         """
         Copy the current posterior to a constant tensor, which will be used as prior for the next task
         """
+        #self.prior = [(mu.clone().detach(), log_sig.clone().detach()) for mu, log_sig in self._get_posterior()]
         self.prior = [(mu.clone().detach(), log_sig.clone().detach()) for mu, log_sig in self._get_posterior()]
+        for mu_sig in self.prior:
+            nn.init.constant_(mu_sig[1], -6.0)
+            
 
     def KL_from_prior(self):
         params = [(*post, *prior) for (post, prior) in zip(self._get_posterior(), self.prior)]
-        KL = torch.zeros(1, device=device)
+        KL = torch.zeros(1, device=device).squeeze() #don't know how to generate a zero scalar
         for param in params:
             unsqueezed_param = list(map(lambda x: x.unsqueeze(0), param))
             tmp = math_utils.KL_div_gaussian(*unsqueezed_param)
             KL += tmp.squeeze()
 
-        return KL.item()
+        return KL
 
     @property
     def d_in(self):
@@ -251,9 +255,8 @@ class TaskModel(nn.Module):
         kl_shared_dec_Qt_2_PREV_Qt = self.dec_shared.KL_from_prior()
 
         if loss_print:
-            print("Log_like", torch.mean(logp))
-            print("KL Z", torch.mean(kl_z))
-            print("KL Qt vs prev Qt", (kl_shared_dec_Qt_2_PREV_Qt / self.DatasetSize))
+            print("Log_like", "\tKL Z","\tKL Qt vs prev Qt")
+            print(int(torch.mean(logp).item()),'\t',int(torch.mean(kl_z).item()), '\t', int(kl_shared_dec_Qt_2_PREV_Qt / self.DatasetSize))
 
         # We ignore the kl(private dec || Normal(0,1) ) like the authors did
         ELBO = torch.mean(logp) - torch.mean(kl_z) - (kl_shared_dec_Qt_2_PREV_Qt / self.DatasetSize)
@@ -279,6 +282,8 @@ class TaskModel(nn.Module):
             print("starting epoch " + str(epoch))
             running_loss = 0.0
             for i, data in enumerate(task_trainloader):
+                global loss_print
+                loss_print = (i % 20 == 19)
                 # get the inputs
                 inputs, labels = data
                 #Migrate to device (gpu if possible)
@@ -292,9 +297,9 @@ class TaskModel(nn.Module):
 
                 # print statistics
                 running_loss += loss.item()  # ?
-                if i % 50 == 49:  # print every 2000 mini-batches
+                if i % 20 == 19:  # print every 2000 mini-batches
                     print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss/50))
+                          (epoch + 1, i + 1, running_loss/20))
                     running_loss = 0.0
         # This will set the prior to the current posterior, before we start to change it during training
         self._update_prior()
@@ -367,8 +372,8 @@ def main():
     task_loaders = zip(create_mnist_single_digit_loaders(batch_size), create_mnist_single_digit_loaders(batch_size, train_data=False))
 
     models = []
-
-    evaluators = [evaluate_YvsX_log_like.Evaluation(),
+    #[evaluate_YvsX_log_like.Evaluation(),
+    evaluators =  [ \
                   EvaluateClassifierUncertainty.EvaluateClassifierUncertainty('./classifier_params')] #classifier is loaded. asssumes already trained
 
     # A task corresponds to a digit